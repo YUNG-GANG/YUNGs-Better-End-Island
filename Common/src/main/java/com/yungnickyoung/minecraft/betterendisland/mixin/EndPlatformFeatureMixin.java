bb8c3e0f91bc92ebdcb5321a25a92672c4d869eb
EEsa khokhar <khokhareesa.home@gmai.com>
2024-07-16 18:48:00 -0400

    1.21

--- Common/src/main/java/com/yungnickyoung/minecraft/betterendisland/mixin/EndPlatformFeatureMixin.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/betterendisland/mixin/EndPlatformFeatureMixin.java
@@ -5,17 +5,20 @@ import com.yungnickyoung.minecraft.betterendisland.world.feature.BetterEndSpawnP
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.level.levelgen.feature.EndPlatformFeature;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ServerPlayer.class)
+@Mixin(EndPlatformFeature.class)
 public abstract class ServerPlayerMixin {
     @Inject(method = "createEndPlatform", at = @At("HEAD"), cancellable = true)
-    private void betterendisland_createEndPlatform(ServerLevel level, BlockPos pos, CallbackInfo ci) {
+    private static void betterendisland_createEndPlatform(ServerLevelAccessor levelAccessor, BlockPos pos, boolean $$2, CallbackInfo ci) {
         if (!BetterEndIslandCommon.CONFIG.useVanillaSpawnPlatform) {
-            BetterEndSpawnPlatformFeature.place(level, pos);
+            BetterEndSpawnPlatformFeature.place(levelAccessor.getLevel(), pos);
             ci.cancel();
         }
     }
